276fea30845c4813139cbd38fef428080b9c2808
saliha <saliha-afzaal@outlook.com>
2023-04-19 14:31:53 +0200

    feat: Implement plot_comparison function using Matplotlib

---
+++ plotting/visualise_prx_comparison.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+def plot_comparison(pr1, pr2, pr3):
+    """
+        Vykresluje srovnání různých metod dokončení pro data PRx.
+
+        Args:
+            pr1 (seznam): Seznam dat PRx vyplněný pomocí prahové metody.
+            pr2 (seznam): Seznam dat PRx vyplněný pomocí lineární interpolace.
+            pr3 (seznam): Seznam dat PRx vyplněný pomocí polynomiální interpolace druhého řádu.
+
+        Vrácení:
+            Žádný
+
+        Poznámka:
+            Tato funkce využívá knihovnu Matplotlib ke generování grafu různých PRx dat a jejich příslušných metod dokončení.
+        """
+    plt.figure(figsize=(25,8))
+    plt.rcParams.update({'font.size': 25})
+    plt.title("Porovnání doplňovacích metod")
+
+    plt.plot(pr1[0:420], color="violet", label="Prahovací metoda", linewidth=2)
+    plt.plot(pr2[0:420], color="#ff7487ff", label="Linearní interpolace", linewidth=3)
+    plt.plot(pr3[0:420],'--', color="4385ffff", label="Interpolace polynomem 2. řádu", linewidth=3)
+    plt.plot(0,60)
+    plt.xlabel("Čas(min)")
+    plt.ylabel("PRx")
+    plt.legend()
+    # plt.savefig("Porovnani_prx.png", dpi=300)
+    plt.show()
